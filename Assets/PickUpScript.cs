0004b3d51fe9be188488844f176c8e6cbb28c0c2
JonKirkland <35732956+JonKirkland@users.noreply.github.com>
2022-08-09 22:02:50 +0100

    Pick up file

---
+++ Assets/PickUpScript.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    public GameObject player;
+    public Transform holdPos;
+
+    private GameObject heldObj;
+    public float pickUpRange = 5f;
+    private float rotationSensitivity = 1f;
+    private Rigidbody heldObjRb;
+
+    private bool canDrop = true;
+    private int LayerNumber;
+    ECM.Components.MouseLook mouseLook;
+    void Start()
+    {
+        LayerNumber = LayerMask.NameToLayer("holdLayer");
+        mouseLook = player.GetComponent<ECM.Components.MouseLook>();     
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObj == null)
+            {
+                print(LayerNumber);
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
+                {
+                    if (hit.transform.gameObject.tag == "canPickUp")
+                    {
+                        PickUpObject(hit.transform.gameObject);                        
+                    }
+                }
+            }
+            else
+            {
+                if(canDrop == true)
+                {
+                    StopClipping();
+                    DropObject();
+                }
+            }
+        }
+        if (heldObj != null)
+        {
+            MoveObject();
+            RotateObject();
+            if (Input.GetKeyDown(KeyCode.Mouse0) && canDrop == true)
+            {               
+                StopClipping();
+                ThrowObject();
+            }
+
+        }
+    }
+    void PickUpObject(GameObject pickUpObj)
+    {
+        if (pickUpObj.GetComponent<Rigidbody>())
+        {
+            heldObj = pickUpObj;
+            heldObjRb = pickUpObj.GetComponent<Rigidbody>();
+            heldObjRb.isKinematic = true;
+            heldObjRb.transform.parent = holdPos.transform;
+            heldObj.layer = LayerNumber;
+            Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), true);
+        }
+    }
+    void DropObject()
+    {
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 0;
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        heldObj = null;
+    }
+    void MoveObject()
+    {
+        heldObj.transform.position = holdPos.transform.position;
+    }
+
+    
+    void RotateObject() 
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            canDrop = false;
+            mouseLook._verticalSensitivity = 0f;
+            mouseLook._lateralSensitivity = 0f;
+            float XaxisRotation = Input.GetAxis("Mouse X") * rotationSensitivity;
+            float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSensitivity;
+
+            heldObj.transform.Rotate(Vector3.down, XaxisRotation);
+            heldObj.transform.Rotate(Vector3.right, YaxisRotation);
+        }
+        else
+        {
+            mouseLook._verticalSensitivity = 2f;
+            mouseLook._lateralSensitivity = 2f;
+            canDrop = true;
+        }
+    }   
+    void ThrowObject()
+    {
+
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 0;
+        heldObjRb.isKinematic = false;
+        heldObjRb.AddForce(transform.forward * 1000);
+        heldObj.transform.parent = null;
+        heldObj = null;
+    }
+    void StopClipping()
+    {
+        var clipRange = Vector3.Distance(heldObj.transform.position,transform.position);
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), clipRange);
+        
+        if (hits.Length > 1)
+        {
+            heldObj.transform.position = transform.position + new Vector3(0f, -0.5f, 0f);
+            print("clip prevented");
+        }
+    }
+}
